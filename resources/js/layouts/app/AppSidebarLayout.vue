5eaccc8223302759b460de7ce68eadaa5fb4670f
ajonmarc <119939476+ajonmarc@users.noreply.github.com>
2026-08-13 10:58:17 +0800

    remove duplicated toast

--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -22,6 +22,6 @@ withDefaults(defineProps<Props>(), {
             <AppSidebarHeader :breadcrumbs="breadcrumbs" />
             <slot />
         </AppContent>
-        <Toaster />
+            <Toaster position="top-right" richColors closeButton :toastOptions="{ style: { borderRadius: '12px' } }" />
     </AppShell>
 </template>
